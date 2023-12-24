c24f5f528e7fd38882e9a60865e03744590b9653
popnitesh <niteshsingh.s3094@gmail.com>
2023-12-24 16:36:23 +0530

    create docker file

---
+++ DockerFile.kt
@@ -0,0 +1,24 @@
+# Use an official base image
+FROM openjdk:8-jdk-alpine
+
+# Set the working directory
+WORKDIR /app
+
+# Copy the Gradle files
+COPY build.gradle settings.gradle gradlew /app/
+COPY gradle /app/gradle
+
+# Copy the source code
+COPY app /app/app
+
+# Build the project
+RUN ./gradlew assembleDebug
+
+# Expose the port if your app uses one
+EXPOSE 8080
+
+# Command to run the application
+CMD ["./gradlew", "installDebug"]
+ENTRYPOINT ["./gradlew", "installDebug"]
+
+
